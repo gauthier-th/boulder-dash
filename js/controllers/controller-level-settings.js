7d3c7d209366fefc6163db43ed2152e4cfdb98c2
Nicooow <nicolas.boujot@icloud.com>
2022-04-07 16:23:27 +0200

    Affichage des niveaux dans l'écran de réglage

--- js/controllers/controller-level-settings.js
+++ js/controllers/controller-level-settings.js
@@ -1,12 +1,28 @@
 import { Subject } from "../../patterns/subject.js";
+import { Level } from "../models/model-level.js";
 
 export class ControllerLevelSettings extends Subject {
 
   #application;
+  #levels = [];
 
   constructor(application) {
     super();
     this.#application = application;
+    this.loadLevels();
+    this.notify();
+  }
+
+  loadLevels(){
+    for (const levelText of this.#application.levels) {
+      const level = new Level(this);
+      level.loadLevelFromText(levelText);
+      this.#levels.push(level);
+    }
+  }
+  
+  get levels(){
+    return this.#levels;
   }
 
 }
--- js/view/view-level-settings.js
+++ js/view/view-level-settings.js
@@ -2,15 +2,40 @@ import { Observer } from "../../patterns/observer.js";
 
 export class ViewLevelSettings extends Observer {
 
-  #controllerMenu;
+  controllerLevelSettings;
 
-  constructor(controllerMenu) {
+  constructor(controllerLevelSettings) {
     super();
-    this.#controllerMenu = controllerMenu;
-    this.#controllerMenu.addObserver(this);
+    this.controllerLevelSettings = controllerLevelSettings;
+    this.controllerLevelSettings.addObserver(this);
+    this.update();
+  }
+
+  showLevels() {
+    const grid = document.getElementById("levels");
+
+    for (const level of this.controllerLevelSettings.levels) {
+      grid.innerHTML += this.formatLevel(level);
+    }
+  }
+
+  formatLevel(level){
+    let html = '<div class="level-grid">';
+    for (let line of level.cells) {
+      let ligneHTML = '<div class="line">';
+      for (let cell of line) {
+        ligneHTML += `<div class="cell cell-${cell.getLetter().toLowerCase()}"></div>`;
+      }
+      ligneHTML += '</div>';
+      html += ligneHTML;
+    }
+    html += '</div>';
+
+    return html;
   }
 
   update() {
+    this.showLevels();
   }
 
 }
